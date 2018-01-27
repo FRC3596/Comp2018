3fe0994d254c4cf04c95ba38883429ad9701a8d3
Khalil Horton <horton.k223@gmail.com>
2018-01-26 18:20:29 -0600

    Added SmartDashboard Modules

--- src/org/usfirst/frc/team3596/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team3596/robot/subsystems/Elevator.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3596.robot.subsystems;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
@@ -33,10 +34,10 @@ public class Elevator extends Subsystem {
     	mainMotor.set(0);
     }
     public void log(){
-    	mainMotor.getSpeed();
-    	top.get();
-    	bottom.get();
-    	middle.get();
+    	SmartDashboard.putNumber("Elevator Motor", mainMotor.getSpeed());
+    	SmartDashboard.putBoolean("TOP SWITCH",top.get());
+    	SmartDashboard.putBoolean("BOTTOM SWITCH", bottom.get());
+    	SmartDashboard.putBoolean("MIDDLE SWITCH", middle.get());
     }
     public boolean isSwitchTrigger(){
     	return (top.get()||bottom.get()||middle.get());
