4f8b9ed000a32fc1fc6836cf18e01673bca6bc82
Josh <Josh@192.168.1.104>
2018-02-13 17:13:09 -0600

    Simplification - Created a speed control group.

--- src/org/usfirst/frc/team3596/robot/subsystems/Elevator.java
+++ src/org/usfirst/frc/team3596/robot/subsystems/Elevator.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3596.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -15,6 +16,9 @@ public class Elevator extends Subsystem {
 	DigitalInput top = new DigitalInput(0);
 	DigitalInput bottom = new DigitalInput(1);
 	DigitalInput middle = new DigitalInput(2);
+	
+	SpeedControllerGroup elevatorMotor = new SpeedControllerGroup(leftMotor, rightMotor);
+	
 
 
 	// Put methods for controlling this subsystem
@@ -25,25 +29,22 @@ public class Elevator extends Subsystem {
 		//setDefaultCommand(new MySpecialCommand());
 	}
 	public void ElevatorDownward(){
-		leftMotor.set(-.5);
-		rightMotor.set(-.5);
+		elevatorMotor.set(-.5);
+		
 	}
 
 	public void ElevatorUpward(){
-		leftMotor.set(.5);
-		rightMotor.set(.5);
+		elevatorMotor.set(.5);
 	}
 
 	public void ElevatorStop(){
-		leftMotor.set(0);
-		rightMotor.set(0);
+		elevatorMotor.set(0);
 	}
 
 	public boolean isSwitchTrigger(){
 		return (top.get() || bottom.get() || middle.get());
 	}
 	public void log(){
-		SmartDashboard.putNumber("Elevator Motor", mainMotor.getSpeed());
 		SmartDashboard.putBoolean("TOP SWITCH",top.get());
 		SmartDashboard.putBoolean("BOTTOM SWITCH", bottom.get());
 		SmartDashboard.putBoolean("MIDDLE SWITCH", middle.get());
