e635b9077d1e2e65b54e53f5bf2f2d78e1e1cecd
Khalil Horton <horton.k223@gmail.com>
2018-02-19 11:55:44 -0600

    Update ElevatorUp.java

--- src/org/usfirst/frc/team3596/robot/commands/ElevatorUp.java
+++ src/org/usfirst/frc/team3596/robot/commands/ElevatorUp.java
@@ -24,14 +24,7 @@ public class ElevatorUp extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		if(Robot.elevator.topCheck())
-    	{
-    		return true;
-    	}
-    	else
-    	{
-    		return false;
-    	}
+		return Robot.elevator.topCheck();
 	}
 
 	// Called once after isFinished returns true
